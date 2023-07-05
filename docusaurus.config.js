b88b67828c51fc7164b495c56b5659ea8ce68f04
Adam Berger <adam@simplystated.dev>
2023-07-05 10:14:37 -0400

    feat: initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,147 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'State Backed',
+  tagline: 'XState backend as a service, up and running in 5 minutes',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://docs.statebacked.dev',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'statebacked', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/statebacked/docs/tree/main/',
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl:
+            'https://github.com/statebacked/docs/tree/main/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'State Backed',
+        logo: {
+          alt: 'State Backed Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'docsSidebar',
+            position: 'left',
+            label: 'Docs',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/statebacked/smply',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: "State Backed",
+                href: "https://statebacked.dev"
+              },
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/statebacked',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/abrgrBuilds',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'Client library',
+                href: 'https://github.com/statebacked/client-js',
+              },
+              {
+                label: 'Token generation library',
+                href: 'https://github.com/statebacked/token',
+              },
+              {
+                label: 'smply CLI',
+                href: 'https://github.com/statebacked/smply',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/statebacked',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © 2023 Simply Stated Software, LLC.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
