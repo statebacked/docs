0f7a57c254c401e69f5c99df8ae61efcab76e5dc
Adam Berger <adam@simplystated.dev>
2023-07-26 17:14:09 -0400

    feat: update social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      image: 'img/social-card.jpg',
+      image: 'img/social-card.png',
       navbar: {
         title: 'State Backed',
         logo: {
