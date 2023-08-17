6030b764eb60efc404ef05553ed8c06105ae8dcc
Adam Berger <adam@simplystated.dev>
2023-08-17 11:51:22 -0400

    feat: homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,6 +34,24 @@ export default function Home(): JSX.Element {
       <HomepageHeader />
       <main>
         <HomepageFeatures />
+        <div className="container" style={{marginTop: 40}}>
+          <div className="text--center">
+            <h1>The fastest path from business logic to production</h1>
+          </div>
+        </div>
+        <div className="container" style={{marginTop: 40}}>
+          <ul className={styles.benefitList}>
+            <li>Deploy any state machine</li>
+            <li>Deploy infinite instances</li>
+            <li>Leverage instance state and context as your storage layer</li>
+            <li>Built-in multiplayer support - connect many clients to one machine instance</li>
+            <li>Built-in realtime support - state updates instantly pushed to all clients</li>
+            <li>Reliable, long-running timers for easy scheduling of jobs in seconds or years</li>
+            <li>Always-on, unkillable instances with no cold starts or server crashes to worry about</li>
+            <li>End-to-end security with simple, fine-grained authorization</li>
+            <li>Automatically expose a simple, consistent, event-based frontend API with beautiful type safety</li>
+          </ul>
+        </div>
       </main>
     </Layout>
   );
