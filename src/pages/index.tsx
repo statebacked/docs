e22dd6e16e0ef5c32709dac666fec4eee2e56e6b
Adam Berger <adam@simplystated.dev>
2023-08-17 15:58:59 -0400

    refactor: fmt

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,29 +59,31 @@ export default function Home(): JSX.Element {
             Deploy your backend for free in &lt;5 mins
           </Link>
         </div>
-        <div className="container text--center" style={{marginTop: 40}}>
-        <div id="mc_embed_shell">
-          <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css" />
-          <style type="text/css">{`
+        <div className="container text--center" style={{ marginTop: 40 }}>
+        </div>
+        <div className="container text--center" style={{ marginTop: 40 }}>
+          <div id="mc_embed_shell">
+            <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css" />
+            <style type="text/css">{`
               #mc_embed_signup{false;clear:left; font:14px Helvetica,Arial,sans-serif; width: 600; margin: auto; max-width: 600px; overflow: hidden;}
               #mc_embed_signup .helper_text { background: none; }
             `}
-          </style>
-          <div id="mc_embed_signup">
-            <form action="https://teampando.us8.list-manage.com/subscribe/post?u=dec43cbfe556d982657100961&amp;id=3616b8513f&amp;f_id=00cb72e0f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank">
-              <div id="mc_embed_signup_scroll"><h2>Stay up to date with the latest from State Backed</h2>
-                <div className="indicates-required"><span className="asterisk">*</span> indicates required</div>
-                <div className="mc-field-group"><label htmlFor="mce-EMAIL">Email Address <span className="asterisk">*</span></label><input type="email" name="EMAIL" className="required email" id="mce-EMAIL" required={true} defaultValue="" /><span id="mce-EMAIL-HELPERTEXT" className="helper_text"></span></div>
-                <div id="mce-responses" className="clear">
-                  <div className="response" id="mce-error-response" style={{ display: "none" }}></div>
-                  <div className="response" id="mce-success-response" style={{ display: "none" }}></div>
-                </div><div aria-hidden="true" style={{ position: "absolute", left: -5000 }}><input type="text" name="b_dec43cbfe556d982657100961_3616b8513f" tabIndex={-1} readOnly value="" /></div><div className="clear"><input type="submit" name="subscribe" id="mc-embedded-subscribe" className="button" value="Subscribe" /></div>
-              </div>
-            </form>
+            </style>
+            <div id="mc_embed_signup">
+              <form action="https://teampando.us8.list-manage.com/subscribe/post?u=dec43cbfe556d982657100961&amp;id=3616b8513f&amp;f_id=00cb72e0f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank">
+                <div id="mc_embed_signup_scroll"><h2>Stay up to date with the latest from State Backed</h2>
+                  <div className="indicates-required"><span className="asterisk">*</span> indicates required</div>
+                  <div className="mc-field-group"><label htmlFor="mce-EMAIL">Email Address <span className="asterisk">*</span></label><input type="email" name="EMAIL" className="required email" id="mce-EMAIL" required={true} defaultValue="" /><span id="mce-EMAIL-HELPERTEXT" className="helper_text"></span></div>
+                  <div id="mce-responses" className="clear">
+                    <div className="response" id="mce-error-response" style={{ display: "none" }}></div>
+                    <div className="response" id="mce-success-response" style={{ display: "none" }}></div>
+                  </div><div aria-hidden="true" style={{ position: "absolute", left: -5000 }}><input type="text" name="b_dec43cbfe556d982657100961_3616b8513f" tabIndex={-1} readOnly value="" /></div><div className="clear"><input type="submit" name="subscribe" id="mc-embedded-subscribe" className="button" value="Subscribe" /></div>
+                </div>
+              </form>
+            </div>
+            <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
+            <script type="text/javascript">{`(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';}(jQuery));var $mcj = jQuery.noConflict(true);`}</script>
           </div>
-          <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
-          <script type="text/javascript">{`(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';}(jQuery));var $mcj = jQuery.noConflict(true);`}</script>
-        </div>
         </div>
       </main>
     </Layout>
